dad95526f2def964a34ed69d40a84e2cf02b4f46
orange <orange@WX-5CG74731LR.Jt.JtGroup>
2023-04-15 16:30:55 +0300

    create quiz pages

---
+++ quiz-prief/ali-daraghmeh/alidaragmeh/quiz3-q5.js
@@ -0,0 +1,25 @@
+//left-side numbers 
+const n1 =document.getElementById("q1");
+const n2 =document.getElementById("q2");
+const n3 =document.getElementById("q3");
+const n4 =document.getElementById("q4");
+const n5 =document.getElementById("q5");
+
+
+//countdown timer
+var timeLeft = 15; // in seconds
+var timerId = setInterval(countdown, 1000);
+
+function countdown() {
+  if (timeLeft == 0) {
+    clearTimeout(timerId);
+   //window.location.href='quiz3-q2.html';  change this 
+  } else {
+    document.getElementById("time-left").innerHTML = timeLeft + "s";
+    timeLeft--;
+  }
+  n1.style.backgroundColor  ="gray";
+  n2.style.backgroundColor  ="gray";
+  n3.style.backgroundColor  ="gray";
+  n4.style.backgroundColor  ="gray";
+}
